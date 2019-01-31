09907028f47898fba0e5e176424f5382757c6d17
Mike Kevin Castro <mikekevincastro.uc@gmail.com>
2019-01-31 17:43:01 +0800

    create unit test

---
+++ tests/Unit/SRTFTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+use App\ShortestRemainingTimeFirst;
+
+class SRTFTest extends BaseTestCase
+{
+    /**
+     * @test
+     * @covers \App\ShortestRemainingTimeFirst
+     */
+    public function execute()
+    {
+        // given
+
+
+        // when
+
+
+        // then
+
+        $this->assert;
+    }
+}
